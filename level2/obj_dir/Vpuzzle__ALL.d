24051e2a67d2ad992e1b990aa94ea7e6eead689f
Gaurav Somani <gsomani@protonmail.com>
2026-08-30 20:50:24 +0530

    remove vpuzzle

--- level2/obj_dir/Vpuzzle__ALL.d
+++
@@ -1,7 +0,0 @@
-Vpuzzle__ALL.o: Vpuzzle__ALL.cpp Vpuzzle.cpp Vpuzzle.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vpuzzle__Syms.h \
- Vpuzzle__Trace.cpp /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h Vpuzzle__Slow.cpp \
- Vpuzzle__Syms.cpp Vpuzzle__Trace__Slow.cpp
